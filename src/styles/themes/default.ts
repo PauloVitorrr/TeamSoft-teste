105480173ef0bde446e08ffebbd5c17ceea31534
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-10-31 19:24:18 -0300

    first commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+  white: "#fff",
+
+  "gray-100": "#F4F4F4",
+  "gray-500": "#4E4E4E",
+  "gray-600": "#656363",
+
+  "red-200": "#ED3237",
+  "red-300": "#ED3237",
+
+  "orange-200": "#F09035",
+} as const;
